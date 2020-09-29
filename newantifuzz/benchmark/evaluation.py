86388b2283eda824fd70458915a2838b5227bdf6
xposimon <472032034@qq.com>
2020-09-29 20:52:32 +0800

    evaluation

--- newantifuzz/benchmark/evaluation.py
+++ newantifuzz/benchmark/evaluation.py
@@ -5,7 +5,8 @@ parallel_num = 2
 execute_one_round = 7
 
 bin_format = '{binname}_{num}_{attype}'
-cmd_format = 'nohup afl-fuzz -Q -i ./{bin_name}/input -o ./{bin_name}/{outputdir} {delay} -%s {taskname}_fuzz%d -- ./{bin_name}/{taskname} {run_cmd} >/dev/null 2>&1 &'
+cmd_formats = ['nohup afl-fuzz -Q -i ./{bin_name}/input -o ./{bin_name}/{outputdir} {delay} -%s {taskname}_fuzz%d -- ./{bin_name}/{taskname} {run_cmd} >/dev/null 2>&1 &', 'nohup aflfast -Q -i ./{bin_name}/input -o ./{bin_name}/{outputdir} {delay} -%s {taskname}_fuzz%d -- ./{bin_name}/{taskname} {run_cmd} >/dev/null 2>&1 &']
+#qsym_cmd_formats = ['nohup afl-fuzz -Q -i ./{bin_name}/input -o ./{bin_name}/{outputdir} {delay} -%s {taskname}_fuzz%d -- ./{bin_name}/{taskname} {run_cmd} >/dev/null 2>&1 &', 'nohup python3 run_qsym_afl.py -a {taskname}_fuzz%d -o ./{bin_name}/{outputdir} -n qsym -- ./{bin_name}/{taskname} {run_cmd} >/dev/null 2>&1 &']
 
 run_cmds = {'readelf':' -a @@', 'objdump':' -d @@', 'nm':' @@', 'objcopy':' -S @@'}
 tasks = {'readelf':[('0','ori', '')] + [(str(i*100), 'funcchain', '') if i < 2 else (str(i*100), 'funcchain', '-t 5000') for i in range(1, 6)] + [('10000', 'atbr', '-t 5000'), ("1000", "2funcchain", '-t 5000'), ("1000", "funcchain", "-t 5000")], 
@@ -17,6 +18,7 @@ tasks = {'readelf':[('0','ori', '')] + [(str(i*100), 'funcchain', '') if i < 2 e
 commands = []
 parallel_commands = []
 
+for cmd_format in cmd_formats:
 for binname in tasks.keys():
     commands = [cmd_format.format(bin_name=binname, outputdir = bin_format.format(binname=binname, num=funcnum, attype=aptype)+'_output', taskname = bin_format.format(binname=binname,num=funcnum, attype=aptype), delay=t, run_cmd=run_cmds[binname]) for funcnum, aptype, t in tasks[binname]]
 

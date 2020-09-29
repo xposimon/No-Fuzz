a2a7d88bfad791afd15b34beaee3469492ba41d9
Ubuntu <ubuntu@ip-172-31-47-121.ap-southeast-1.compute.internal>
2020-09-29 13:32:51 +0000

    eva

--- newantifuzz/benchmark/evaluation.py
+++ newantifuzz/benchmark/evaluation.py
@@ -5,26 +5,26 @@ parallel_num = 2
 execute_one_round = 7
 
 bin_format = '{binname}_{num}_{attype}'
-cmd_formats = ['nohup afl-fuzz -Q -i ./{bin_name}/input -o ./{bin_name}/{outputdir} {delay} -%s {taskname}_fuzz%d -- ./{bin_name}/{taskname} {run_cmd} >/dev/null 2>&1 &', 'nohup aflfast -Q -i ./{bin_name}/input -o ./{bin_name}/{outputdir} {delay} -%s {taskname}_fuzz%d -- ./{bin_name}/{taskname} {run_cmd} >/dev/null 2>&1 &']
+cmd_formats = ['nohup afl-fuzz -Q -i ./{bin_name}/input -o ./{bin_name}/{outputdir}{fuzzerid} {delay} -%s {taskname}_fuzz%d -- ./{bin_name}/{taskname} {run_cmd} >/dev/null 2>&1 &', 'nohup aflfast -Q -i ./{bin_name}/input -o ./{bin_name}/{outputdir}{fuzzerid} {delay} -%s {taskname}_fuzz%d -- ./{bin_name}/{taskname} {run_cmd} >/dev/null 2>&1 &']
 #qsym_cmd_formats = ['nohup afl-fuzz -Q -i ./{bin_name}/input -o ./{bin_name}/{outputdir} {delay} -%s {taskname}_fuzz%d -- ./{bin_name}/{taskname} {run_cmd} >/dev/null 2>&1 &', 'nohup python3 run_qsym_afl.py -a {taskname}_fuzz%d -o ./{bin_name}/{outputdir} -n qsym -- ./{bin_name}/{taskname} {run_cmd} >/dev/null 2>&1 &']
 
 run_cmds = {'readelf':' -a @@', 'objdump':' -d @@', 'nm':' @@', 'objcopy':' -S @@'}
-tasks = {'readelf':[('0','ori', '')] + [(str(i*100), 'funcchain', '') if i < 2 else (str(i*100), 'funcchain', '-t 5000') for i in range(1, 6)] + [('10000', 'atbr', '-t 5000'), ("1000", "2funcchain", '-t 5000'), ("1000", "funcchain", "-t 5000")], 
-'objdump':[('0', 'ori', ''), ('100', '0funcchain', ''), ('500', '0funcchain', ' -t 5000'), ('1000', '2funcchain', ' -t 5000')],
-'nm':[('0', 'ori', ''), ('100', 'funcchain', ''), ('500', 'funcchain', ' -t 5000'), ('1000', 'funcchain', ' -t 5000')],
-'objcopy':[('0', 'ori', ''), ('100', 'funcchain', ''), ('500', 'funcchain', ' -t 5000'), ('1000', 'funcchain', ' -t 5000')]
+tasks = {'readelf':[('0','ori', '')] + [(str(i*100), 'funcchain', '') if i < 2 else (str(i*100), 'funcchain', '-t 5000') for i in (1,4,5,10)] + [('10000', 'atbr', '-t 5000')], 
+'objdump':[('0', 'ori', ''), ('100', 'funcchain', ''), ('500', 'funcchain', ' -t 5000'), ('1000', 'funcchain', ' -t 5000'), ('10000', 'atbr', '-t 5000')],
+'nm':[('0', 'ori', ''), ('100', 'funcchain', ''), ('500', 'funcchain', ' -t 5000'), ('1000', 'funcchain', ' -t 5000'), ('10000', 'atbr', '-t 5000')],
+'objcopy':[('0', 'ori', ''), ('100', 'funcchain', ''), ('500', 'funcchain', ' -t 5000'), ('1000', 'funcchain', ' -t 5000'), ('10000', 'atbr', '-t 5000')]
 }
 
 commands = []
 parallel_commands = []
 
-for cmd_format in cmd_formats:
-for binname in tasks.keys():
-    commands = [cmd_format.format(bin_name=binname, outputdir = bin_format.format(binname=binname, num=funcnum, attype=aptype)+'_output', taskname = bin_format.format(binname=binname,num=funcnum, attype=aptype), delay=t, run_cmd=run_cmds[binname]) for funcnum, aptype, t in tasks[binname]]
+for fuzzerid, cmd_format in enumerate(cmd_formats): 
+    for binname in tasks.keys():
+        commands = [cmd_format.format(bin_name=binname, outputdir = bin_format.format(binname=binname, num=funcnum, attype=aptype)+'_output', taskname = bin_format.format(binname=binname,num=funcnum, attype=aptype), delay=t, run_cmd=run_cmds[binname], fuzzerid=fuzzerid) for funcnum, aptype, t in tasks[binname]]
 
     # AFL parallel
-    for i in range(len(commands)):
-        parallel_commands += [commands[i]%(host_type, id) for host_type, id in [('M',0)] + [('S', j) for j in range(1, parallel_num)] ]
+        for i in range(len(commands)):
+            parallel_commands += [commands[i]%(host_type, id) for host_type, id in [('M',0)] + [('S', j) for j in range(1, parallel_num)] ]
 
 cnt = 0
 while cnt*parallel_num*execute_one_round < len(parallel_commands):
@@ -38,16 +38,20 @@ while cnt*parallel_num*execute_one_round < len(parallel_commands):
     time.sleep(5)
     ps = subprocess.check_output(["ps", "-aux"])
     process = subprocess.Popen(["grep", "afl-fuzz"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    process2 = subprocess.Popen(["grep", "aflfast"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     #print(process, ps)
     data, err = process.communicate(ps)
-    check_num = len(data.decode().split("\n"))
-    if check_num != correct_check_num + 1:
+    data2, _ = process2.communicate(ps)
+    check_num = len(data.decode().split("\n")) - 1
+    check_num += len(data2.decode().split("\n")) - 1
+    if check_num != correct_check_num:
         os.system("pkill afl-fuzz")
-        print(check_num, correct_check_num+1)
+        os.system("pkill aflfast")
+        print(check_num, correct_check_num)
         time.sleep(10)
         print("[-] Restart!")
         continue
-    print(check_num, correct_check_num + 1)
+    print(check_num, correct_check_num)
 
     start = time.time()
     cnt += 1
@@ -56,6 +60,7 @@ while cnt*parallel_num*execute_one_round < len(parallel_commands):
         if now - start > last_time:
             print("[*] End")
             os.system("pkill afl-fuzz")
+            os.system("pkill aflfast")
             time.sleep(2)
             break
         else:

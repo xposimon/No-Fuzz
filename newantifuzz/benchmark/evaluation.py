9e32e454ac952ffb467cdc790ee59ec579e0288c
Ubuntu <ubuntu@ip-172-31-47-121.ap-southeast-1.compute.internal>
2020-10-05 12:08:33 +0000

    eval

--- newantifuzz/benchmark/evaluation.py
+++ newantifuzz/benchmark/evaluation.py
@@ -28,35 +28,47 @@ for fuzzerid, cmd_format in enumerate(cmd_formats):
             for i in range(len(commands)):
                 parallel_commands += [commands[i]%(host_type, id) for host_type, id in [('M',0)] + [('S', j) for j in range(1, parallel_num)] ]
         if fuzzerid == 2:
-            parallel_commands += [cmd_format.format(bin_name=binname, outputdir = bin_format.format(binname=binname, num=funcnum, attype=aptype)+'_output', taskname = bin_format.format(binname=binname,num=funcnum, attype=aptype), run_cmd=run_cmds[binname].repace("@@", "___FILE___"), fuzzerid=fuzzerid)]
-        
+            tmp = [cmd_format.format(bin_name=binname, outputdir = bin_format.format(binname=binname, num=funcnum, attype=aptype)+'_output', taskname = bin_format.format(binname=binname,num=funcnum, attype=aptype), run_cmd=run_cmds[binname].replace("@@", "___FILE___"), fuzzerid=fuzzerid) for funcnum, aptype, t in tasks[binname]]
+            tmp = [[tmp[i]] + ['' for j in range(parallel_num-1)]  for i in range(len(tmp))]
+            final = []
+            for i in range(len(tmp)):
+                final += tmp[i]
+            parallel_commands += final
+
 
 cnt = 0
 while cnt*parallel_num*execute_one_round < len(parallel_commands):
     cmd = parallel_commands[cnt*parallel_num*execute_one_round:(cnt+1)*parallel_num*execute_one_round]
     correct_check_num = len(cmd)
+    cmd = [cmd[i] for i in range(len(cmd)) if cmd[i] != '']
+    #print(len(cmd))
     cmd = '\n'.join(cmd)
     time.sleep(2)
     print("[*] %d-%d:"%(cnt*parallel_num*execute_one_round, (cnt+1)*parallel_num*execute_one_round))
     print(cmd)
     os.system(cmd+"\n")
+    
     time.sleep(5)
-    #ps = subprocess.check_output(["ps", "-aux"])
-    #process = subprocess.Popen(["grep", "afl-fuzz"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    #process2 = subprocess.Popen(["grep", "aflfast"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    ps = subprocess.check_output(["ps", "-aux"])
+    process = subprocess.Popen(["grep", "afl-fuzz"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    process2 = subprocess.Popen(["grep", "aflfast"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    process3 = subprocess.Popen(["grep", "honggfuzz "], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     #print(process, ps)
-    #data, err = process.communicate(ps)
-    #data2, _ = process2.communicate(ps)
-    #check_num = len(data.decode().split("\n")) - 1
-    #check_num += len(data2.decode().split("\n")) - 1
-    #if check_num != correct_check_num:
-    #    os.system("pkill afl-fuzz")
-    #    os.system("pkill aflfast")
-    #    print(check_num, correct_check_num)
-    #    time.sleep(10)
-    #    print("[-] Restart!")
-    #    continue
-    #print(check_num, correct_check_num)
+    data, err = process.communicate(ps)
+    data2, _ = process2.communicate(ps)
+    data3, _ = process3.communicate(ps)
+    check_num = len(data.decode().split("\n")) - 1
+    check_num += len(data2.decode().split("\n")) - 1
+    check_num += (len(data3.decode().split("\n")) - 1) * parallel_num
+    if check_num != correct_check_num:
+        os.system("pkill afl-fuzz")
+        os.system("pkill aflfast")
+        os.system("pkill honggfuzz")
+        print(check_num, correct_check_num)
+        time.sleep(10)
+        print("[-] Restart!")
+        continue
+    print(check_num, correct_check_num)
 
     start = time.time()
     cnt += 1
@@ -66,6 +78,7 @@ while cnt*parallel_num*execute_one_round < len(parallel_commands):
             print("[*] End")
             os.system("pkill afl-fuzz")
             os.system("pkill aflfast")
+            os.system("pkill honggfuzz")
             time.sleep(2)
             break
         else:
